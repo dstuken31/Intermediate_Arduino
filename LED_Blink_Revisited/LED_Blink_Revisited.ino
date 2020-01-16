523b2cfb693bd8bc3755ae2e1c158caa85086619
dstuken31 <56082602+dstuken31@users.noreply.github.com>
2020-01-16 18:05:26 -0500

    Added comments to LED Blink Revisited

--- LED_Blink_Revisited/LED_Blink_Revisited.ino
+++ LED_Blink_Revisited/LED_Blink_Revisited.ino
@@ -6,7 +6,7 @@ void setup() {
 }
 
 void loop() {
-  // fade in from min to max in increments of 5 points:
+  // fade in from min to max 5 points at a time:
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
     // sets the value (range from 0 to 255):
     analogWrite(ledPin, fadeValue);
@@ -17,7 +17,7 @@ void loop() {
     delay(30);
 }
 
-  // fade out from max to min in increments of 5 points:
+  // fade out from max to min 5 points at a time 
   for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
     // sets the value (range from 0 to 255):
     analogWrite(ledPin, fadeValue);
