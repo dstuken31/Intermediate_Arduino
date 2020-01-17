764aa8dc27046ce64989b2040d89cea6a4c05f8f
dstuken31 <56082602+dstuken31@users.noreply.github.com>
2020-01-17 11:11:00 -0500

    Added in comments on top of code

--- LCD_Backpack/Pushbutton_LCD_Backpack.ino
+++ LCD_Backpack/Pushbutton_LCD_Backpack.ino
@@ -1,3 +1,9 @@
+// LCD Backpack 
+// Dylan Stukenberg
+// Incredibly Import line of Code (if(buttonState == HIGH && previousbuttonState == LOW)) 
+// If Loops https://www.arduino.cc/reference/en/language/structure/control-structure/if/
+// LCD Backpack https://www.adafruit.com/product/292
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h> 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display.  
